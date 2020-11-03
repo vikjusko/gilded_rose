c68dd844004f68313ded872088cee6a1872bba9e
Victoria Jusko <vikjusko@gmail.com>
2020-11-03 14:53:59 +0000

    refactor or mathematical statements in code

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,25 +10,25 @@ class GildedRose
 			if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality -= 1
           end
         end
 			else
 				#if it is the Brie, backstage pass pr Sulurass, and the quality is less than < 50
 				# item quality increases by +1
         if item.quality < 50
-					item.quality = item.quality + 1
+					item.quality += 1
 					#if item is a backstage pass for a concert that is in less than 11 days, 
 					#and the quality is less than 50, increase the quality by 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
           end
@@ -42,15 +42,15 @@ class GildedRose
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                item.quality -= 1
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality -= item.quality
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            item.quality += 1
           end
         end
       end
