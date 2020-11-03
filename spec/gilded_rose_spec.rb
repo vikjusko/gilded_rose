b3c33ee74568f6e8e01f75905de1d0a3e088b467
Victoria Jusko <vikjusko@gmail.com>
2020-11-03 17:37:01 +0000

    Organised test in a logical flow

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -16,10 +16,25 @@ describe GildedRose do
 				expect(items[0].quality).to eq 1
 		end 
 
+		it "qeeps quality of Brie the same when its quanitity is 50" do 
+			items = [Item.new(name = "Aged Brie", sell_in = 2, quality = 50)]
+			GildedRose.new(items).update_quality
+			expect(items[0].sell_in).to eq 1
+			expect(items[0].quality).to eq 50
+		end
+
 		context "increases the quality of Backstage passes the closer the concert is" do
-			it "increases the backstage pass only by 1 because it goes over the quality of 50" do
+
+			it "increases the backstage pass only by 3 because it is less than 5 days away from the concert" do
+				items = [Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 5, quality = 40)]
+				#it only goes up by one because the quality cannot be more than 50
+				GildedRose.new(items).update_quality
+				expect(items[0].sell_in).to eq 4
+				expect(items[0].quality).to eq 43
+			end
+			
+			it "increases the backstage pass only by 1  when sell by date is less than 6 because it goes over the quality of 50" do
 			items = [ Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 5, quality = 49)]
-			#it only goes up by one because the quality cannot be more than 50
 			GildedRose.new(items).update_quality
 			expect(items[0].sell_in).to eq 4
 			expect(items[0].quality).to eq 50
@@ -40,13 +55,6 @@ describe GildedRose do
 				expect(items[0].quality).to eq 50
 			end
 
-			it "increases the backstage pass only by 3 because it is less than 5 days away from the concert" do
-				items = [Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 5, quality = 40)]
-				#it only goes up by one because the quality cannot be more than 50
-				GildedRose.new(items).update_quality
-				expect(items[0].sell_in).to eq 4
-				expect(items[0].quality).to eq 43
-			end
 		end 
   end
 end
